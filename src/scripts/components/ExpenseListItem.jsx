108d3d6bfa673ec9b26a9908913a6630bf278da1
tim1016 <inkant@gmail.com>
2019-02-16 22:31:26 -0600

    Probably unfixed date issue

--- src/scripts/components/ExpenseListItem.jsx
+++ src/scripts/components/ExpenseListItem.jsx
@@ -6,7 +6,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt}) => (
     <div key={id}>
         <NavLink activeClassName="is-active" to={`/edit/${id}`}  exact={true}> <h3>{description} </h3> </NavLink>
         <p>Amount: {amount}</p>  
-        <p>Date: {moment.unix(createdAt).format('YYYY, MMM, Do')}</p>  
+        <p>{amount} - {createdAt}</p>
     </div>
 );
 
