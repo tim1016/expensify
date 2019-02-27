2ec6b04e4eb518df2c3496dac171d1ec0c14fa5f
tim1016 <inkant@gmail.com>
2019-02-27 16:21:09 -0600

    login/logout implemented

--- src/scripts/tests/components/Header.test.js
+++ src/scripts/tests/components/Header.test.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import Header from '../../components/Header';
+import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import { Header } from '../../components/Header';
 
-test('Header shallow render', ()=>{
-    const wrapper = shallow (<Header/>);
-
-    // You can do similar DOM selection as in regular javascript like below
-    // .find is like .querySelector
-    // expect(wrapper.find('#myElementId').text()).toBe('Expensify');
-
-
-    expect(toJSON(wrapper)).toMatchSnapshot();
-
+test('Header shallow render', () => {
+  const onClickSpy = jest.fn();
+  const wrapper = shallow(<Header startLogout={onClickSpy}/>);
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
 
-
-
+test('Header Logout button spy', () => {
+  const onClickSpy = jest.fn();
+  const wrapper = shallow(<Header startLogout={onClickSpy} />);
+  wrapper.find('button').simulate('click');
+  expect(onClickSpy).toHaveBeenCalled();
+});
--- src/scripts/tests/components/LoginPage.test.js
+++ src/scripts/tests/components/LoginPage.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import LoginPage from '../../components/LoginPage'; 
+import { LoginPage } from '../../components/LoginPage';
 
 test('Shallow render Login page', () => {
-  const wrapper = shallow(<LoginPage />);
+  const onClickSpy = jest.fn();
+  const wrapper = shallow(<LoginPage onClick={onClickSpy} />);
   expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+test('Header Logout button spy', () => {
+  const onClickSpy = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={onClickSpy} />);
+  wrapper.find('button').simulate('click');
+  expect(onClickSpy).toHaveBeenCalled();
 });
